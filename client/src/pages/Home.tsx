a19860041d9da1f0918f1e94a42f7311b3705c0b
Kevin <kg71875@gmail.com>
2025-01-08 15:03:35 +0100

    Create component cards hour/hour whith CSS style

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,10 @@
+import Cardhours from "../components/Cardhours";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Cardhours />
+    </>
+  );
 }
