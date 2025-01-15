e2720d6f68991860b950894cb629632da3763c13
Clément.A <125081819+ITClmt@users.noreply.github.com>
2025-01-15 10:10:28 +0100

    Reorder imports in Home component for better organization

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,7 @@
-import Cardhours from "../components/Cardhours";
-import "../components/MainCard";
 import CardAlert from "../components/CardAlert";
+import Cardhours from "../components/Cardhours";
 import MainCard from "../components/MainCard";
 import WeekTemp from "../components/WeekTemp";
-import { useWeather } from "../components/WeatherContext";
 
 export default function Home() {
   return (
